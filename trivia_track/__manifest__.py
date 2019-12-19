3b071b9fb5408c08a697c07586e6775fc05fb7ff
mdark1001 <miguel.cabrera.app@gmail.com>
2019-12-19 12:31:20 -0600

    [REF] Dependencias

--- trivia_track/__manifest__.py
+++ trivia_track/__manifest__.py
@@ -17,7 +17,8 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base',
-                'generar_folios'
+                'generardor_folio'
+                ],
                 ],
     'data': [
         'security/ir.model.access.csv',
